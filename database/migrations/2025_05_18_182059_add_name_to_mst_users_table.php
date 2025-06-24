e2cc2f592c2c465d84372b9655c28f63cc59d1f9
Afdhal <13020230069@student.umi.ac.id>
2025-06-24 10:39:30 +0800

    chore: optimize for railway deployment

--- database/migrations/2025_05_18_182059_add_name_to_mst_users_table.php
+++ database/migrations/2025_05_18_182059_add_name_to_mst_users_table.php
@@ -1,8 +1,28 @@
 <?php
 
-public function up()
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
 {
-    Schema::table('mst_users', function (Blueprint $table) {
-        $table->string('name')->after('id'); // atau posisi yang diinginkan
-    });
-}
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('mst_users', function (Blueprint $table) {
+            $table->string('name')->after('id'); // atau posisi yang diinginkan
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('mst_users', function (Blueprint $table) {
+            $table->dropColumn('name');
+        });
+    }
+};
